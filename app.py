97094c6a54925eae1dd501e0bc00ad5dee47a505
markfr7 <mf@imaginedata.com>
2023-05-02 14:43:16 -0700

    commit 20230502

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask
 import json
 from flask import render_template, jsonify
-
+from os.path import exists
 
 app = Flask(__name__)
 
@@ -54,18 +54,20 @@ def destination(name=None):
     #deserialize
     data = json.loads(dat)
     #make lists to send to template
+    #description, to-do and to-see lists use '||' to separate paragraphs/items
     file_list = data["img-filenames"].split(",")
-    todo_list = data["to-do-list"].split(",")
-    tosee_list = data["to-see-list"].split(",")
+    todo_list = data["to-do-list"].split("||")
+    tosee_list = data["to-see-list"].split("||")
     try:
         links_list = data["links"].split(",")
     except:
         links_list = []
-    print(links_list)
-    #description uses '||' to separate paragraphs
+    # print(links_list)
     descrip = data["description"].split("||")
-
-    return render_template('destination.html', name=name, data=data, img_files=file_list, paras=descrip, todos=todo_list, tosees=tosee_list, links=links_list)
+    #check for map file
+    file_exists = exists('static/img/' + name + '/' + name + '-map2.jpg')
+    # print(file_exists)
+    return render_template('destination.html', name=name, data=data, img_files=file_list, paras=descrip, todos=todo_list, tosees=tosee_list, links=links_list, map_file = file_exists)
 
 
 @app.route('/page/')
@@ -80,4 +82,4 @@ def test():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=4999)
